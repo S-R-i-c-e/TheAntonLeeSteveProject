ff5c0d1b416b4ac6835bb1c7f6348ec358788dc6
Lee Vine <36503381+leedvine@users.noreply.github.com>
2023-02-09 19:30:46 +0000

    Update holiday.js

--- assets/js/holiday.js
+++ assets/js/holiday.js
@@ -152,14 +152,7 @@ function retrieveNationalHoidays(countryCode) {
         fetch(createHolidaysRequestURL(countryCode))                // create request URL string
             .then(response => response.json())
             .then(holidayData => processHolidays(holidayData));     // pass data on for processing
-    } else {                                                    // if it is known,
-    if (!knownHolidays) { 
-        console.log("www");                                          // if not, fetch it from the calendarific api
-        fetch(createHolidaysRequestURL(countryCode))                // create request URL string
-            .then(response => response.json())
-            .then(holidayData => processHolidays(holidayData));     // pass data on for processing
-    } else {  
-        console.log("local")                                                      // if it is known,
+    } else {                                                     // if it is known,
         displayHolidays(knownHolidays);                             // then display
     }
 }
